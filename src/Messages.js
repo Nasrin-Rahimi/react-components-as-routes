569df4911f0696b2bb35414029e890e8235f3091
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-03 13:25:03 -0400

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Messages extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Messages</h1>
+                <p>The First message</p>
+                <p>The Second Message</p>
+            </div>
+         )
+    }
+}
+
+export default Messages
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 
-const Home = () => {
-  return (
+
+
+
+const Signup = () => 
+  <form>
+    <h1>Sign Up</h1>
     <div>
-      <h1>Home!</h1>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
     </div>
-  );
-};
+    <div>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
+    </div>
+    <input type="submit" value="Sign Up" />
+  </form>;
 
-ReactDOM.render(
-  <Home />,
+  const Messages = () => 
+    <div>
+      <h1>Messages</h1>
+      <p>The First message</p>
+      <p>The Second Message</p>
+    </div>
+
+ReactDOM.render((
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/messages" component={Messages} />
+    </div>
+  </Router>),
   document.getElementById('root')
-);
+); 
